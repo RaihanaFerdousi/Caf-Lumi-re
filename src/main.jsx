2e53432a6aeff476c869749af14aca5a1c1beda8
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2025-02-13 11:13:00 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
